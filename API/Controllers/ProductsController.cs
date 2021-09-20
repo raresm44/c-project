2199685916ae5b27ae6a760c7e9d4285c8a52d25
Vasile Rares <raresm44@gmail.com>
2021-09-20 17:05:49 +0300

    adding Dependecy Injection(DI) to ProductsController

--- API/Controllers/ProductsController.cs
+++ API/Controllers/ProductsController.cs
@@ -1,6 +1,7 @@
 using System.Collections.Generic;
 using System.Threading.Tasks;
 using Core.Entities;
+using Core.Interfaces;
 using Infrastructure.Data;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
@@ -12,18 +13,19 @@ namespace API.Controllers
 
     public class ProductsController : ControllerBase
     {
-        private readonly StoreContext _context;
+        private readonly IProductRepository _repo;
 
-        public ProductsController(StoreContext context)
+        public ProductsController(IProductRepository repo)
         {
-            _context = context;
+            
+            _repo = repo;
         }
 
 
         [HttpGet]
          public async Task<ActionResult<List<Product>>> GetProducts(){
              
-             var products = await _context.Products.ToListAsync();
+             var products = await _repo.GetProductsAsync();
 
              return Ok(products);
          }
@@ -32,7 +34,7 @@ namespace API.Controllers
         [HttpGet("{id}")]
         public async Task<ActionResult<Product>> GetProduct(int id)
         {
-            return await _context.Products.FindAsync(id);
+            return await _repo.GetProductByIdAsync(id);
         }
     }
 }
